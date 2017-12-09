f9b102849de0d306288dacee7811979341b21b55
Stephen Lin <sevenlol@Stephende-MacBook-Pro.local>
2017-12-09 18:36:37 +0800

    Implement initial layout of intro component

---
+++ web/src/app/home/intro/intro.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-intro',
+  templateUrl: './intro.component.html',
+  styleUrls: ['./intro.component.css']
+})
+export class IntroComponent implements OnInit {
+
+  imageUrl: string = 'https://avatars3.githubusercontent.com/u/6497884?s=400&u=14a9be7c3afc0a1e0d797db1d9a7c86c5aac7a83&v=4';
+  name: string = 'Stephen Lin';
+  summary: string = 'Backend Developer';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
