7fa9fea2045c2e0796d8483804fa13a65cf8e06e
Stephen Lin <sevenlol@Stephende-MacBook-Pro.local>
2017-12-09 18:37:27 +0800

    Add contact info component with social links

---
+++ web/src/app/home/contact-info/contact-info.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-info',
+  templateUrl: './contact-info.component.html',
+  styleUrls: ['./contact-info.component.css']
+})
+export class ContactInfoComponent implements OnInit {
+
+  profiles: object = {
+    facebook : {
+      url : 'https://www.facebook.com/fattselin',
+      username : 'fattselin'
+    },
+    github : {
+      url : 'https://github.com/sevenlol',
+      username : 'sevenlol'
+    },
+    twitter : {
+      url : 'https://twitter.com/sevenlol1007',
+      username : 'sevenlol1007'
+    }
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
