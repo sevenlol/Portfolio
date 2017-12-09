083d47a0d63b27dd021b39b0dba8235c8c2a95cd
Stephen Lin <sevenlol@Stephende-MacBook-Pro.local>
2017-12-09 18:37:54 +0800

    Initialize featured project component (not implemented yet)

---
+++ web/src/app/home/featured-projects/featured-projects.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-featured-projects',
+  templateUrl: './featured-projects.component.html',
+  styleUrls: ['./featured-projects.component.css']
+})
+export class FeaturedProjectsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
