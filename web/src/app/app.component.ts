e80a621d97201f21715e5cbcd0007faae55dea33
sevenlol <sevenlol1007@gmail.com>
2017-12-12 14:48:30 +0800

    Display spinner in the home page when metadata is loading

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,7 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/distinctUntilChanged';
 
-import { MainMetadata } from './core/metadata.model';
-import { MetadataService } from './core/metadata.service';
+import { Component, OnInit } from '@angular/core';
+import {
+  Router,
+  RouterEvent,
+  NavigationStart,
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError
+} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,14 +19,25 @@ import { MetadataService } from './core/metadata.service';
 })
 export class AppComponent implements OnInit {
 
-  metadataLoaded: boolean = false;
+  showSpinner: boolean = false;
 
-  constructor(private metaService: MetadataService) { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
-    this.metaService.getMainMetadata().subscribe((res) => {
-      console.log(res);
-      this.metadataLoaded = (res !== undefined);
-    });
+    this.router.events
+      .filter(e => this.isStart(e) || this.isEnd(e))
+      .map(e => this.isStart(e))
+      .distinctUntilChanged()
+      .subscribe((isStart) => this.showSpinner = isStart);
+  }
+
+  private isStart(e: any): boolean {
+    return e instanceof NavigationStart;
+  }
+
+  private isEnd(e: any): boolean {
+    return (e instanceof NavigationEnd) ||
+      (e instanceof NavigationCancel) ||
+      (e instanceof NavigationError);
   }
 }
--- web/src/app/core/core.module.ts
+++ web/src/app/core/core.module.ts
@@ -4,11 +4,16 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { environment } from '../../environments/environment';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatButtonModule } from '@angular/material/button';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SpinnerComponent } from './spinner/spinner.component';
 import { MetadataService } from './metadata.service';
+import { InfoService } from './info/info.service';
 import { MainMetadataResolverService } from './main-metadata-resolver.service';
+import { KeywordMetadataResolverService } from './keyword-metadata-resolver.service';
+import { BasicInfoResolverService } from './info/basic-info-resolver.service';
 
 @NgModule({
   imports: [
@@ -16,16 +21,21 @@ import { MainMetadataResolverService } from './main-metadata-resolver.service';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     MatToolbarModule,
+    MatProgressSpinnerModule,
     MatButtonModule,
     FlexLayoutModule
   ],
   exports : [
-    NavBarComponent
+    NavBarComponent,
+    SpinnerComponent
   ],
   providers : [
     MetadataService,
-    MainMetadataResolverService
+    InfoService,
+    MainMetadataResolverService,
+    KeywordMetadataResolverService,
+    BasicInfoResolverService
   ],
-  declarations: [NavBarComponent]
+  declarations: [NavBarComponent, SpinnerComponent]
 })
 export class CoreModule { }
