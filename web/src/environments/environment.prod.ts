768b59fca8394e0ecf7d150cd0b3e0be00c53201
sevenlol <sevenlol1007@gmail.com>
2017-12-15 11:20:17 +0800

    Accidentally upload this, does not matter much but should not be uploaded

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyB1Ngo0P2RHJHCwAArXNQnJ7Z71jmmBws0",
-    authDomain: "sevenlolportfolio.firebaseapp.com",
-    databaseURL: "https://sevenlolportfolio.firebaseio.com",
-    projectId: "sevenlolportfolio",
-    storageBucket: "sevenlolportfolio.appspot.com",
-    messagingSenderId: "565666800633"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   }
 };
