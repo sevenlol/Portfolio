79a5dc9bad5780e4fbf98a18242631ea562454e6
sevenlol <sevenlol1007@gmail.com>
2017-12-15 15:05:45 +0800

    [core] Add unit tests for main/keyword metadata resolver service

--- web/src/app/core/keyword-metadata-resolver.service.spec.ts
+++ web/src/app/core/keyword-metadata-resolver.service.spec.ts
@@ -1,15 +1,68 @@
-import { TestBed, inject } from '@angular/core/testing';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/delay';
+import { TestBed, inject, async } from '@angular/core/testing';
 
+import { Observable } from 'rxjs/Observable';
+
+import { MetadataService } from './metadata.service';
+import { MainMetadata, KeywordMetadata } from './metadata.model';
 import { KeywordMetadataResolverService } from './keyword-metadata-resolver.service';
+
+const KEYWORD_METADATA: KeywordMetadata = {
+  keywords : {
+    angular : {
+      displayName : 'Angular'
+    },
+    firebase : {
+      displayName : 'Firebase'
+    },
+    firestore : {
+      displayName : 'Firestore'
+    },
+    material : {
+      displayName : 'Material'
+    }
+  }
+};
 
 describe('KeywordMetadataResolverService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [KeywordMetadataResolverService]
+      providers: [
+        KeywordMetadataResolverService,
+        {
+          provide : MetadataService,
+          useValue : new MetadataServiceStub(KEYWORD_METADATA, 1000)
+        }
+      ]
     });
   });
 
   it('should be created', inject([KeywordMetadataResolverService], (service: KeywordMetadataResolverService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should resolve after metadata emit', async(inject([KeywordMetadataResolverService], (service: KeywordMetadataResolverService) => {
+    expect(service).toBeTruthy();
+
+    service.resolve(null, null).subscribe((metadata) => {
+      expect(metadata).toBe(KEYWORD_METADATA);
+    });
+  })));
 });
+
+class MetadataServiceStub {
+  constructor(
+    private keyword: KeywordMetadata,
+    private delay: number) {
+  }
+
+  getMainMetadata(): Observable<MainMetadata> {
+    return ;
+  }
+
+  getKeywordMetadata(): Observable<KeywordMetadata> {
+    return Observable.of(this.keyword).delay(this.delay);
+  }
+}
+
--- web/src/app/core/main-metadata-resolver.service.spec.ts
+++ web/src/app/core/main-metadata-resolver.service.spec.ts
@@ -1,15 +1,63 @@
-import { TestBed, inject } from '@angular/core/testing';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/delay';
 
+import { TestBed, inject, async } from '@angular/core/testing';
+import { Observable } from 'rxjs/Observable';
+
+import { MetadataService } from './metadata.service';
+import { MainMetadata, KeywordMetadata } from './metadata.model';
 import { MainMetadataResolverService } from './main-metadata-resolver.service';
+
+const MAIN_METADATA: MainMetadata = {
+  languages : {
+    javascript : {
+      displayName : 'Javascript'
+    }
+  },
+  types : {
+    web : {
+      displayName : 'Web'
+    }
+  }
+};
 
 describe('MetadataResolverService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [MainMetadataResolverService]
+      providers: [
+        MainMetadataResolverService,
+        {
+          provide : MetadataService,
+          useValue : new MetadataServiceStub(MAIN_METADATA, 1000)
+        }
+      ]
     });
   });
 
   it('should be created', inject([MainMetadataResolverService], (service: MainMetadataResolverService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should resolve after metadata emit', async(inject([MainMetadataResolverService], (service: MainMetadataResolverService) => {
+    expect(service).toBeTruthy();
+
+    service.resolve(null, null).subscribe((metadata) => {
+      expect(metadata).toBe(MAIN_METADATA);
+    });
+  })));
 });
+
+class MetadataServiceStub {
+  constructor(
+    private main: MainMetadata,
+    private delay: number) {
+  }
+
+  getMainMetadata(): Observable<MainMetadata> {
+    return Observable.of(this.main).delay(this.delay);
+  }
+
+  getKeywordMetadata(): Observable<KeywordMetadata> {
+    return null;
+  }
+}
