c2c0158a146b7cf99d4f0e3d543fce08c4c8f2a7
sevenlol <sevenlol1007@gmail.com>
2017-12-15 14:08:27 +0800

    [core][nav-bar] Simple component creation unit test

--- web/src/app/core/nav-bar/nav-bar.component.spec.ts
+++ web/src/app/core/nav-bar/nav-bar.component.spec.ts
@@ -1,4 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { NavBarComponent } from './nav-bar.component';
 
@@ -8,6 +13,13 @@ describe('NavBarComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports : [
+        // TODO maybe mock it so that no http request is made (?)
+        HttpClientModule,
+        RouterTestingModule,
+        MatIconModule,
+        MatToolbarModule
+      ],
       declarations: [ NavBarComponent ]
     })
     .compileComponents();
