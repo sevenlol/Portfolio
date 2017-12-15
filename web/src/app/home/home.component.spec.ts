15976f75c6cc5d20bb0bc9926ff357a74a03c312
sevenlol <sevenlol1007@gmail.com>
2017-12-15 11:17:30 +0800

    [home] Add simple unit tests for HomeComponent creation
    1. Mock activated route data

--- web/src/app/home/home.component.spec.ts
+++ web/src/app/home/home.component.spec.ts
@@ -1,6 +1,20 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { MatIconModule } from '@angular/material/icon';
+
+import { BasicInfo } from '../core/info/info.model';
+import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { Observable } from 'rxjs/Observable';
+
+const BASIC_INFO: BasicInfo = {
+  name : 'Stephen Lin',
+  summary : 'Backend Developer',
+  image : 'https://avatars3.githubusercontent.com/u/6497884?s=400&u=14a9be7c3afc0a1e0d797db1d9a7c86c5aac7a83&v=4',
+  profile : {}
+};
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +22,22 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      imports : [
+        MatIconModule,
+        HomeRoutingModule
+      ],
+      providers : [
+        {
+          provide : ActivatedRoute,
+          useValue : {
+            data : Observable.of({
+              basicInfo : BASIC_INFO
+            })
+          }
+        }
+      ],
+      declarations: [ HomeComponent ],
+      schemas : [ CUSTOM_ELEMENTS_SCHEMA ]
     })
     .compileComponents();
   }));
