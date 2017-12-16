6de72ecaf9e145e82869da506763d4d0a3c1344f
sevenlol <sevenlol1007@gmail.com>
2017-12-16 14:45:42 +0800

    [core] Add unit tests for PageNotFoundComponent

--- web/src/app/core/page-not-found/page-not-found.component.spec.ts
+++ web/src/app/core/page-not-found/page-not-found.component.spec.ts
@@ -1,13 +1,27 @@
+import { DebugElement } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { MatIconModule } from '@angular/material/icon';
+import { By } from '@angular/platform-browser';
 
 import { PageNotFoundComponent } from './page-not-found.component';
+
+const ERROR_MESSAGES = {
+  TITLE : 'Oops!',
+  CONTENT : 'The page you are looking for cannot be found!'
+};
 
 describe('PageNotFoundComponent', () => {
   let component: PageNotFoundComponent;
   let fixture: ComponentFixture<PageNotFoundComponent>;
+  let de: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports : [
+        MatIconModule,
+        HttpClientModule
+      ],
       declarations: [ PageNotFoundComponent ]
     })
     .compileComponents();
@@ -16,10 +30,26 @@ describe('PageNotFoundComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PageNotFoundComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should dispaly error icon', () => {
+    let errorEle = de.query(By.css('mat-icon[svgIcon="error"]'));
+    expect(errorEle).toBeTruthy();
+  });
+
+  it('should display error messages', () => {
+    let titleEle = de.query(By.css('h1'));
+    expect(titleEle).toBeTruthy();
+    expect(titleEle.nativeElement.textContent).toContain(ERROR_MESSAGES.TITLE);
+
+    let contentEle = de.query(By.css('p'));
+    expect(contentEle).toBeTruthy();
+    expect(contentEle.nativeElement.textContent).toContain(ERROR_MESSAGES.CONTENT);
   });
 });
