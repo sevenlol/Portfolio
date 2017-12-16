ed2aee3b1cf31b92d9cebbcd2e11ef5ab4db2673
sevenlol <sevenlol1007@gmail.com>
2017-12-16 11:09:15 +0800

    [core][info] Add basic unit tests for info resolver service

--- web/src/app/core/info/basic-info-resolver.service.spec.ts
+++ web/src/app/core/info/basic-info-resolver.service.spec.ts
@@ -1,15 +1,48 @@
-import { TestBed, inject } from '@angular/core/testing';
+import 'rxjs/add/observable/of';
+import { TestBed, inject, async } from '@angular/core/testing';
+import { Observable } from 'rxjs/Observable';
 
+import { BasicInfo } from './info.model';
+import { InfoService } from './info.service';
 import { BasicInfoResolverService } from './basic-info-resolver.service';
+
+const BASIC_INFO: BasicInfo = {
+  name : 'Stephen Lin',
+  summary : 'Java Backend Developer',
+  image : 'https://www.google.com',
+  profile : {}
+};
 
 describe('BasicInfoResolverService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [BasicInfoResolverService]
+      providers: [
+        {
+          provide : InfoService,
+          useValue : new InfoServiceStub(BASIC_INFO);
+        },
+        BasicInfoResolverService
+      ]
     });
   });
 
   it('should be created', inject([BasicInfoResolverService], (service: BasicInfoResolverService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should return the correct basic info', async(inject([BasicInfoResolverService], (service: BasicInfoResolverService) => {
+    expect(service).toBeTruthy();
+
+    service.resolve(null, null).subscribe((info) => {
+      expect(info).toBe(BASIC_INFO);
+    });
+  })));
 });
+
+class InfoServiceStub {
+  constructor(private info: BasicInfo) {}
+
+  getBasicInfo(): Observable<BasicInfo> {
+    return Observable.of(this.info);
+  }
+}
