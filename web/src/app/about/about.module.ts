78ec1168eccee364afab8a679dd09851a9b4bc2a
sevenlol <sevenlol1007@gmail.com>
2017-12-17 22:20:30 +0800

    [about] Add BasicInfoComponent to main about page

--- web/src/app/about/about.module.ts
+++ web/src/app/about/about.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+
 import { AboutRoutingModule } from './about-routing.module';
 import { AboutComponent } from './about.component';
+import { BasicInfoComponent } from './basic-info/basic-info.component';
+import { MatButtonModule, MatTooltipModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatCardModule,
+    MatListModule,
+    MatIconModule,
+    MatTooltipModule,
     AboutRoutingModule
   ],
-  declarations: [AboutComponent]
+  declarations: [AboutComponent, BasicInfoComponent]
 })
 export class AboutModule { }
