65afce4b1a6a353c94db79d100eefb19ec3b614f
sevenlol <sevenlol1007@gmail.com>
2017-12-18 12:19:21 +0800

    [about] Add icons for email/location/phone info item

--- web/src/app/about/basic-info/basic-info.component.ts
+++ web/src/app/about/basic-info/basic-info.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-
-import { Info } from '../../core/info/info.model';
+import { Component, OnInit, Input } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { MatIconRegistry } from '@angular/material';
+
+import { Info } from '../../core/info/info.model';
 
 @Component({
   selector: 'app-basic-info',
@@ -11,31 +11,7 @@ import { MatIconRegistry } from '@angular/material';
 })
 export class BasicInfoComponent implements OnInit {
 
-  // FIXME remove testing data
-  info: Info = {
-    name : 'Stephen Lin',
-    label : 'Backend Developer',
-    summary : 'Backend Developer',
-    image : 'https://avatars3.githubusercontent.com/u/6497884?s=400&u=14a9be7c3afc0a1e0d797db1d9a7c86c5aac7a83&v=4',
-    profile : {
-      github : {
-        url : 'https://github.com/sevenlol',
-        username : 'sevenlol'
-      },
-      linkedin : {
-        url : 'https://www.linkedin.com/in/stephen-lin-b211aa109/',
-        username : 'test'
-      },
-      facebook : {
-        url : 'https://www.facebook.com/fattselin',
-        username : 'test'
-      },
-      twitter : {
-        url : 'https://twitter.com/sevenlol1007',
-        username : 'test'
-      }
-    }
-  };
+  @Input() info: Info;
 
   constructor(
     private iconRegistry: MatIconRegistry,
@@ -47,6 +23,10 @@ export class BasicInfoComponent implements OnInit {
     this.registerIcon('github', 'assets/icons/github.svg');
     this.registerIcon('linkedin', 'assets/icons/linkedin.svg');
     this.registerIcon('twitter', 'assets/icons/twitter.svg');
+
+    this.registerIcon('location', 'assets/icons/location.svg');
+    this.registerIcon('email', 'assets/icons/email.svg');
+    this.registerIcon('phone', 'assets/icons/phone.svg');
   }
 
   private registerIcon(name: string, path: string) {
