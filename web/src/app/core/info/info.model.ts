b84be9dfe223a21b48b03efb9bfca8b6f8e430f4
sevenlol <sevenlol1007@gmail.com>
2017-12-18 12:17:31 +0800

    Implement full info model and modify info service accordingly

--- web/src/app/core/info/info.model.ts
+++ web/src/app/core/info/info.model.ts
@@ -1,4 +1,11 @@
 export interface Info extends BasicInfo {
+  email: string;
+  location: Location;
+  phone: string;
+  resumeUrl: string;
+  url: string;
+  education: Education[];
+  work?: WorkExperience[];
 }
 
 export interface BasicInfo {
@@ -16,4 +23,28 @@ export interface Profile {
 export interface Contact {
   url: string;
   username?: string;
+}
+
+export interface Location {
+  city: string;
+  region: string;
+}
+
+export interface Education {
+  area: string;
+  institution: string;
+  studyType: string;
+  startDate: Date;
+  endDate: Date;
+}
+
+export interface WorkExperience {
+  name: string;
+  location: string;
+  position: string;
+  summary: string;
+  description: string;
+  startDate: Date;
+  endDate: Date;
+  highlights: string[];
 }
--- web/src/app/core/info/info.service.ts
+++ web/src/app/core/info/info.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
 
-import { BasicInfo } from './info.model';
+import { BasicInfo, Info } from './info.model';
 
 const INFO_COLL = 'info';
 const BASIC_INFO_DOC = 'basic';
@@ -13,13 +13,19 @@ export class InfoService {
   public static readonly BASIC_INFO_PATH = `${INFO_COLL}/${BASIC_INFO_DOC}`;
 
   private basicInfoDoc: AngularFirestoreDocument<BasicInfo>;
+  private infoDoc: AngularFirestoreDocument<Info>;
 
   constructor(private afs: AngularFirestore) {
     this.basicInfoDoc = afs.doc(InfoService.BASIC_INFO_PATH);
+    this.infoDoc = afs.doc(InfoService.BASIC_INFO_PATH);
   }
 
   // TODO add projection when the sdk supports it
   getBasicInfo(): Observable<BasicInfo> {
     return this.basicInfoDoc.valueChanges();
+  }
+
+  getInfo(): Observable<Info> {
+    return this.infoDoc.valueChanges();
   }
 }
