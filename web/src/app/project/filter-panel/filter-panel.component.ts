588dac374587bec67066bbb20f3f3b30a0cb593e
sevenlol <sevenlol1007@gmail.com>
2017-12-21 15:03:10 +0800

    [project] Implement layout for FilterPanelComponent
    1. category/language page
    2. clear filter option functions

--- web/src/app/project/filter-panel/filter-panel.component.ts
+++ web/src/app/project/filter-panel/filter-panel.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import { Type, Language, Keyword } from '../../core/metadata.model';
+import { Query, QueryType } from '../project.service';
+
 @Component({
   selector: 'app-filter-panel',
   templateUrl: './filter-panel.component.html',
@@ -7,10 +10,64 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class FilterPanelComponent implements OnInit {
 
+  @Input() languages: Language;
+  @Input('types') categories: Type;
+  @Input() keywords: Keyword;
   @Input() expanded: boolean = false;
+
+  QueryType = QueryType;
+  categoryOptions = [];
+  keywordOptions = [];
+  languageOptions = [];
+
+  type: QueryType;
+  active: boolean;
+  value: string;
+
+  projectStatus;
 
   constructor() { }
 
   ngOnInit() {
+    this.generateOptions(this.categoryOptions, this.categories);
+    this.generateOptions(this.keywordOptions, this.keywords);
+    this.generateOptions(this.languageOptions, this.languages);
+  }
+
+  public apply() {
+    // TODO implement
+  }
+
+  public clearProjectStatus() {
+    delete this.active;
+  }
+
+  public clearFilterOptions() {
+    delete this.type;
+    delete this.active;
+    delete this.value;
+  }
+
+  // FIXME remove hard-coded index comparison
+  public queryTypeChanged(index: number) {
+    if (index === 0) {
+      this.type = QueryType.KEYWORD;
+    } else if (index === 1) {
+      this.type = QueryType.LANGUAGE;
+    } else if (index === 2) {
+      this.type = QueryType.TYPE;
+    }
+    // clear filter options
+    this.clearFilterOptions();
+  }
+
+  private generateOptions(options, map: Type | Keyword | Language) {
+    for (let key in map) {
+      options.push({
+        key : key,
+        name : map[key].displayName,
+        description : map[key].description
+      });
+    }
   }
 }
