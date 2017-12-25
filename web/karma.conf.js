3b53210a4594c23bafddd93534178593a9042da6
sevenlol <sevenlol1007@gmail.com>
2017-12-25 18:57:49 +0800

    Add junit reporter for karma tests

--- web/karma.conf.js
+++ web/karma.conf.js
@@ -9,6 +9,7 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
+      require('karma-junit-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
     ],
@@ -19,10 +20,15 @@ module.exports = function (config) {
       reports: [ 'html', 'lcovonly' ],
       fixWebpackSourcePaths: true
     },
+    junitReporter : {
+      outputDir: (process.env.JUNIT_REPORT_PATH || './reports'),
+      outputFile: (process.env.JUNIT_REPORT_NAME || 'test_report.xml'),
+      useBrowserName: false
+    },
     angularCli: {
       environment: 'dev'
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'junit'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
